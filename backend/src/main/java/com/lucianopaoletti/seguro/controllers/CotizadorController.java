012483086dc6097276f957ca3c02328f84a00a54
Luciano Paoletti <lucianopaoletti19@gmail.com>
2024-01-22 10:54:05 -0500

    Correccion CORS con gateway

--- backend/src/main/java/com/lucianopaoletti/seguro/controllers/CotizadorController.java
+++ backend/src/main/java/com/lucianopaoletti/seguro/controllers/CotizadorController.java
@@ -3,7 +3,6 @@ package com.lucianopaoletti.seguro.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,7 +31,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
-@CrossOrigin
 @Tag(name = "Cotizador", description = "Endpoints necesarios para cotizador")
 @RestController
 @RequestMapping("/cotizador/vehiculos")
